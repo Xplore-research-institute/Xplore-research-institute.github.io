6d8c1ff6591b17d06b70dc66b597d74254129366
mahosain <149421609+mahosain@users.noreply.github.com>
2026-08-01 18:27:51 -0700

    Delete assets directory
    Remove old website files

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.nav-toggle');
-const nav = document.querySelector('.nav-links');
-if (toggle && nav) {
-  toggle.addEventListener('click', () => {
-    const open = nav.classList.toggle('open');
-    toggle.setAttribute('aria-expanded', String(open));
-  });
-}
